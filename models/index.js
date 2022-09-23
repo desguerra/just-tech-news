8b43407afb55bea0a28a4c0fbc505385ffa8955a
desguerra <ddesguerra@ucdavis.edu>
2022-09-23 13:17:37 -0700

    create post feature

--- models/index.js
+++ models/index.js
@@ -1,4 +1,14 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// create associations, a user can make many posts
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+// create reverse association, a post can belong to ONE user
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
