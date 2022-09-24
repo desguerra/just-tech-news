f068261e20b0a4b9d2d652bb9f4b16322702e182
desguerra <ddesguerra@ucdavis.edu>
2022-09-24 00:30:46 -0700

    create and manipulate comments

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require('./User');
 const Post = require('./Post');
 const Vote = require('./Vote');
+const Comment = require('./Comment');
 
 // create associations, a user can make many posts
 User.hasMany(Post, {
@@ -42,4 +43,24 @@ Post.hasMany(Vote, {
     foreignKey: 'post_id',
 });
 
-module.exports = { User, Post, Vote };
+// Note that we don't have to specify Comment as a through table 
+// like we did for Vote. This is because we don't need to access 
+// Post through Comment; we just want to see the user's comment and 
+// which post it was for. Thus, the query will be slightly different.
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+});
+
+module.exports = { User, Post, Vote, Comment };
