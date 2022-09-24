380f8daa73c94c1039f4ef665f0e92836d6e58dd
desguerra <ddesguerra@ucdavis.edu>
2022-09-23 23:47:08 -0700

    refactor messy post code

--- models/index.js
+++ models/index.js
@@ -1,9 +1,10 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
 
 // create associations, a user can make many posts
 User.hasMany(Post, {
-    foreignKey: 'user_id'
+    foreignKey: 'user_id',
 });
 
 // create reverse association, a post can belong to ONE user
@@ -11,4 +12,34 @@ Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
 
-module.exports = { User, Post };
+// many-to-many associations, allowing both the User and Post
+// models to query each other's information in the context of a vote
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id',
+});
+
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id',
+});
+
+Vote.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id',
+});
+
+User.hasMany(Vote, {
+    foreignKey: 'user_id',
+});
+
+Post.hasMany(Vote, {
+    foreignKey: 'post_id',
+});
+
+module.exports = { User, Post, Vote };
